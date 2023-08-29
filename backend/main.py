27a44e4bbd41cfb8aa1d4e1472c9422d520f164c
Ben BL Lee 李家禎 <BenBLLee@corpnet.auo.com>
2023-08-29 10:15:41 +0800

    解bug讓跳頁時選定的時區時間及辦公室都可維持。初步在後端加入api讓地址可搜尋並回傳座標的功能

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+# 用來取得座標
+import requests
+import os
+os.environ['REQUESTS_CA_BUNDLE'] = "TMGCert.crt"                                                # 認證(for nominatim)
+os.environ['http_proxy'] = "http://benbllee:Aa0937454850*@auhqproxy.cdn.corpnet.auo.com:8080"   # proxy
+os.environ['https_proxy'] = "https://benbllee:Aa0937454850*@auhqproxy.cdn.corpnet.auo.com:8080" # proxy
+
 app = FastAPI()
 
-# 這邊的設定是讓後端可以接受前端的request
+# 讓後端可以接受前端的request
 origins = [
    "http://localhost:5173", # vue跑在本地的位置
 ]
 
-# 如何讓fastapi讀進origins的設定
+
+
+# fastapi讀進origins的設定
 app.add_middleware(
    CORSMiddleware,
    allow_origins=origins,
@@ -21,18 +30,28 @@ app.add_middleware(
 @app.get("/latlng_search/{search_keywords}")
 def read_address(search_keywords: str):
 
-   test_company = search_keywords
-   test_lat = 0
-   test_lng = 0
+   nominatim_api_url = 'https://nominatim.openstreetmap.org/search'
+   params = {'q': search_keywords, 'format': 'json'}
 
-   company = test_company
-   lat = test_lat
-   lng = test_lng
-   return {"company": f"{company}",
-           "lat": f"{lat}",
-           "lng": f"{lng}"}
+   response = requests.get( nominatim_api_url, params=params)
 
+   response_json = response.json()
 
+   if response_json:
+      lat = response_json[0]['lat']
+      lng = response_json[0]['lon']
+      company = response_json[0]['display_name']
+
+      return {"status": "ˊsuccess",
+               "company": f"{company}",
+               "lat": f"{lat}",
+               "lng": f"{lng}"}
+
+   else:
+      return {"status": "fail",
+               "company": "",
+               "lat": "",
+               "lng": ""}
 
 
 
